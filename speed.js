f0de21a39063eb7ede0df029af995f191dd974d9
mztc <michal.zak@tiscali.cz>
2026-09-05 16:11:29 +0200

    Delete speed.js

--- speed.js
+++
@@ -1,7 +0,0 @@
-let speedBeforePause = 1;
-function syncSpeedLabel() {
-const el = $("bt-speed");
-el && (el.textContent = "Speed " + (simSpd === 0 ? speedBeforePause : simSpd) + "×");
-const st = $("bt-pause");
-st && (st.textContent = simSpd === 0 ? "\u25b6 PLAY" : "\u275a\u275a PAUSE")
-}
